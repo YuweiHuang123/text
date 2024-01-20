565fb77e88584fcd3151638c0793d0d3656a050d
Hyw <hyw@pc-75-179.customer.ask4.lan>
2024-01-20 15:36:21 +0000

    text

---
+++ test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 20 15:10:59 2024
+
+@author: hyw
+"""
+
+
+import pandas as pd
+
+
+
+data_articles = pd.read_csv("articles.CDKN2A.csv")
+
+data_authors = pd.read_csv("authors.CDKN2A.csv")
+
+data_counts = pd.read_csv("paper_counts.csv")
+
+#print(data_articles)
+
+data_counts.plot()
